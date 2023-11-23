f9f2537c86a1b8f63665477d3235801f4a2e0b0b
Stephany <136421961+splacito22@users.noreply.github.com>
2023-11-22 17:37:53 -0800

    ini commit. set up boilerplate, instal dependencies, moved original html into new project

--- wk-3-mini-pro/script.js
+++ wk-3-mini-pro/script.js
@@ -1,13 +1,49 @@
-var gon = {
-  choices: ["r", "p", "s"],
-};
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var messageEl = document.querySelector("#message");
 
-var choice = prompt("Select one of the following choices: r, p ,s");
+var options = ["R", "P", "S"];
+function playGame() {
+  var userChoice = window.prompt("Enter R, P, or S:");
 
-alert();
+  if (!userChoice) {
+    console.log("Game ended.");
+    messageEl.textContent = "The game was ended. Have a nice day!";
+    return;
+  }
 
-function game() {
-  prompt("Select one of the following choices: r, p ,s");
+  userChoice = userChoice.toUpperCase();
+  var computerChoice = options[Math.floor(Math.random() * options.length)];
+
+  window.alert("The computer chose " + computerChoice);
+
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!");
+  } else {
+    losses++;
+    window.alert("You lost!");
+  }
+
+  window.alert(
+    "Stats: Wins: " + wins + " Losses: " + losses + " Ties: " + ties
+  );
+
+  var playAgain = window.confirm("Play again?");
+
+  if (playAgain) {
+    playGame();
+  }
+
+  messageEl.textContent = "Thank you for playing!";
 }
 
-game();
+playGame();
